3e9ed78fcdd3b21b312db5f23efea359939caf43
jungminim <jungminim@mz.co.kr>
2023-02-24 17:45:10 +0900

    logger add & pipe line test

--- src/main/java/com/ljm/uil/UilApplication.java
+++ src/main/java/com/ljm/uil/UilApplication.java
@@ -12,7 +12,7 @@ public class UilApplication {
 
 	public static void main(String[] args) {
 
-		logger.debug("start application");
+		logger.debug("start application !!!!!!!");
 
 		SpringApplication.run(UilApplication.class, args);
 	}
