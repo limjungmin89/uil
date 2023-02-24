26f465de99f99cdc360e4db7121e76e6ced1ac69
jungminim <jungminim@mz.co.kr>
2023-02-24 17:10:06 +0900

    first commit

---
+++ src/test/java/com/ljm/uil/UilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ljm.uil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
