07de1397d2d6e0bf6d18d5f0d3cae2625e42eca4
jungminim <jungminim@mz.co.kr>
2023-02-24 17:43:42 +0900

    logger add & pipe line test

--- src/main/java/com/ljm/uil/UilApplication.java
+++ src/main/java/com/ljm/uil/UilApplication.java
@@ -1,12 +1,19 @@
 package com.ljm.uil;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class UilApplication {
 
+	static Logger logger = LogManager.getLogger(UilApplication.class);
+
 	public static void main(String[] args) {
+
+		logger.debug("start application");
+
 		SpringApplication.run(UilApplication.class, args);
 	}
 
